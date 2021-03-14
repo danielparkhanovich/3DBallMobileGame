0654993cf3a3e1133b47889a43c298e2f73e3f11
scanax2 <scanax2@gmail.com>
2021-03-15 00:32:27 +0100

    Commit of the latest changes

---
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGeneration : MonoBehaviour
+{
+    [SerializeField] private Transform center;
+
+    // Render parameters
+    [SerializeField] private float renderRadius;
+
+
+    [SerializeField] private GameObject pillarObj;
+
+    // Pillars generation parameters
+    [SerializeField] private float pillarsRingStep;
+    [SerializeField] private float pillarsСloseness;
+    [SerializeField] private float pillarsFrequency;   // e.g. 5 -> 360/5 = 72, pillar on every 72 deg
+
+    [SerializeField] private Vector2 pillarsFloorSize;
+    [SerializeField] private Vector2 pillarsBodyHeight;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
