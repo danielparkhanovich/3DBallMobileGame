ffb714362d6e24df4e1fd5b10bf35cd3e3f042b2
scanax2 <scanax2@gmail.com>
2021-03-15 00:17:34 +0100

    Added: ball movement, ball control for windows.

---
+++ Assets/Scripts/Control/HandleWindows.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleWindows : MonoBehaviour, IHandleInput
+{
+    public bool isLeft()
+    {
+        if (Input.GetKey("a"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool isRight()
+    {
+        if (Input.GetKey("d"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool isDoubleJump()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public float getSingleAngleRotate()
+    {
+        return 0.5f;
+    }
+}
---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    enum Platform
+    {
+        Windows,
+        Android,
+        IOS
+    }
+    [SerializeField] private Platform curentPlatform;
+    [SerializeField] private HandleWindows handleWindows;
+
+    private IHandleInput handleInput;
+    private Rigidbody rb;
+
+    [SerializeField] private float speedOfRotate;
+    [SerializeField] private float speedOfForwardMovement;
+
+    private float currentRotateDirection = 0.0f;
+    private bool isBounce = false;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bounce")
+        {
+            Debug.Log("Bounce !");
+            isBounce = true;
+        }
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        handleWindows = GetComponent<HandleWindows>();
+
+        if (curentPlatform == Platform.Windows)
+        {
+            handleInput = handleWindows;
+        }
+    }
+
+    void manageInput()
+    {
+        if (handleInput.isLeft())
+        {
+            currentRotateDirection -= speedOfRotate * Time.deltaTime * handleInput.getSingleAngleRotate();
+            if (currentRotateDirection < 0)
+            {
+                currentRotateDirection += 2 * Mathf.PI;
+            }
+        }
+        else if (handleInput.isRight())
+        {
+            currentRotateDirection += speedOfRotate * Time.deltaTime * handleInput.getSingleAngleRotate();
+            if (currentRotateDirection > 2 * Mathf.PI)
+            {
+                currentRotateDirection -= 2 * Mathf.PI;
+            }
+        }
+    }
+
+    void Update()
+    {
+        manageInput();
+        transform.eulerAngles = new Vector3(0, currentRotateDirection * (180 / Mathf.PI), 0);
+
+        // Movement 
+        rb.velocity = new Vector3(
+            transform.forward.x * speedOfForwardMovement,
+            rb.velocity.y,
+            transform.forward.z * speedOfForwardMovement);
+
+        isBounce = false;
+    }
+}
